46506dd760045b7d5e82f4585ded289157d6d42b
Maurits Wilke <mauritswilke@gmail.com>
2024-05-01 21:25:56 +0200

    feat: :sparkles: added prohibited ID's from a list of profanity (I found this online, don't blame me for the contents)

--- src/utils.ts
+++ src/utils.ts
@@ -1,12 +1,15 @@
+import { readFileSync } from "fs";
+const prohibitedIDs = readFileSync("./prohibitedIDs.txt", "utf-8").split("\n");
+
 const randomInt = (min: number, max: number) => Math.floor(Math.random() * (max - min + 1) + min);
 
 export function makeLobbyID(length = 5) {
     const CHARSET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
     let result = "";
-    for (let i = 0; i < length; i++) {
-        const randomChar = CHARSET.charAt(randomInt(0, CHARSET.length - 1));
-        result += randomChar;
+
+    while (!result.length || prohibitedIDs.includes(result)) {
+        for (let i = 0; i < length; i++)
+            result += CHARSET.charAt(randomInt(0, CHARSET.length - 1));
     }
 
     return result;
